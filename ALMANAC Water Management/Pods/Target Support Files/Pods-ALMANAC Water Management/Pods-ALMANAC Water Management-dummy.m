e2251712a41b0088b6ccf56da4f7590a4dfe9615
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2016-08-02 13:29:45 +0200

    Update pod

---
+++ ALMANAC Water Management/Pods/Target Support Files/Pods-ALMANAC Water Management/Pods-ALMANAC Water Management-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ALMANAC_Water_Management : NSObject
+@end
+@implementation PodsDummy_Pods_ALMANAC_Water_Management
+@end
---
+++ ALMANAC Water Management/Pods/Target Support Files/Pods-ALMANAC Water Management/Pods-ALMANAC Water Management-umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double Pods_ALMANAC_Water_ManagementVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ALMANAC_Water_ManagementVersionString[];
+
